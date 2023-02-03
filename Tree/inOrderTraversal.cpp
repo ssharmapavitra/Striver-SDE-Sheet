3b51c8c1a7e4a37bade6367b42a24ff7b2cec7b3
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-02-03 14:40:04 +0530

    In Order Traversal

---
+++ Tree/inOrderTraversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Left Root Right
+void inOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+    inOrder(root);
+    return 0;
+}
+
+/*
+        1
+      2   3
+            5
+*/
